eed216c2197f27a11347e8187056800b407d85b3
Pallavi Sharma <pallavisharma@steptoe-10-222-14-72.resnet.wsu.edu>
2022-12-05 01:30:22 -0800

    adding destination changes

---
+++ src/components/bottomBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="row fixed-bottom" id="bottomBarStyle">
+        <div class="col-4">
+             <i class="bi bi-hand-index" style="font-size:3.5rem;"></i>
+        </div>
+        <div class="col-4">
+             <i class="bi bi-mic-fill" style="font-size:3.5rem;"></i>
+        </div>
+        <div class="col-4">
+            <i class="bi bi-chat" style="font-size:3.5rem;"></i>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'bottomBar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#bottomBarStyle{
+    padding-bottom: 20;
+}
+</style>
---
+++ src/components/topBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row sticky-top" id="topBarStyle">
+        <div class="col-3">
+            <h1 id="theia_topbar"> Theia</h1>
+        </div>
+        <div class="col-6">
+        </div>
+        <div class="col-3" id="settingsIcon">
+           <a> <i class="bi bi-gear" style="font-size: 2.5rem;"></i> </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'topBar',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#theia_topbar{
+  font-weight: normal;
+  font-family: 'Snell Roundhand';
+  font-size: 55;
+}
+#topBarStyle{
+    padding-top: 20;
+    padding-bottom: 20;
+}
+#settingsIcon{
+    padding-top: 10;
+}
+</style>
