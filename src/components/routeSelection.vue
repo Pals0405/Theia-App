caae77b922f84f701eaddb34ed43e444a5c1d476
Afoke Abogidi <afoke.abogidi@wsu.edu>
2022-12-06 22:49:12 -0800

    more styling

--- src/components/routeSelection.vue
+++ src/components/routeSelection.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="container-fluid">
         <topBar/>
-        <div class="row">
+        <div class="chat-container">
+            <div class="row">
         <div class="col-3"></div>
         <div class="col-9">
           <p class="chat_dest text-left"> You have chosen Sloan Hall Room 101 as your destination </p>
@@ -17,6 +18,27 @@
             <p class="chat_dest text-left"> Make a route selection</p>
         </div>
         </div>
+        <div>
+            <div class="col-6"><p class="user_chat_dest text-left"> Route A </p></div>
+            <div class="row">
+        <div class="col-3"></div>
+        <div class="col-9">
+          <p class="chat_dest text-left"> Alright!<br>Navigating to your destination via Route A </p>
+          <p class="chat_dest text-left">Keep walking straight. There is a right turn in approximately 20 feet</p>
+          <p class="chat_dest text-left"> Slow down, be prepared to take the right turn in 10, 9, 8, 7, 6, 5, 4, 3, 2, and 1 </p>
+        </div>
+        </div>
+        <div class="col-6"><p class="user_chat_dest text-left">Alright! </p></div>
+        <div class="row">
+            <div class="col-3"></div>
+        <div class="col-9">
+          <p class="chat_dest text-left"> Make a right turn now</p>
+          <p class="chat_dest text-left">You are now approaching your destination</p>
+          <p class="chat_dest text-left"> Slow down, you have now reached Sloan Hall Room 101. </p>
+        </div>
+        </div>
+        </div>
+        </div>        
         <bottomBar/>
     </div>    
 </template>
